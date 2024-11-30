aa35b207aaba19cd7f2271df5e6fce81bf6c2796
Albin Axtelius <albin@axtelius.se>
2024-11-30 15:00:54 +0100

    add test function

--- lib/Runner.ex
+++ lib/Runner.ex
@@ -8,4 +8,14 @@ defmodule Runner do
 
     apply(solution_module, :start, [])
   end
+
+  @spec test(non_neg_integer(), non_neg_integer(), non_neg_integer()) :: nil
+  def test(day, part, year \\ DateTime.utc_now().year)
+
+  def test(day, part, year) do
+    solution_module =
+      :"Elixir.AdventOfCode.Solutions.Year#{year}.Day#{day}.Part#{part}"
+
+    apply(solution_module, :test, [])
+  end
 end
